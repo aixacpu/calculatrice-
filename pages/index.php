dc45c1e5021988000b5213674e8911226e9509c5
Melforsy03 <forsythemelani51@gmail.com>
2025-01-16 22:51:41 -0500

    logueo

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,9 @@
 <?php 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header('Location: login.php');
+    exit();
 }
 ?>
 <!DOCTYPE html>
